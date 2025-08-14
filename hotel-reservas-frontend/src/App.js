4a6361d3fbfc93276fbd3f572b0fe855d631bc03
Cristhian Sánchez <csanchezc5@est.ups.edu.ec>
2025-08-14 01:34:46 -0500

    Frontend usuarios

--- hotel-reservas-frontend/src/App.js
+++ hotel-reservas-frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Habitaciones from './pages/Habitaciones';
 import Reportes from './pages/Reportes';
+import Usuarios from './pages/Usuarios';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/habitaciones" element={<Habitaciones />} />
           <Route path="/reportes" element={<Reportes />} />
+          <Route path="/usuarios" element={<Usuarios />} />
         </Routes>
       </div>
     </Router>
